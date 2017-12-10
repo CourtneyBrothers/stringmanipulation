45b4e821a3ffea2a0fc676fba2426eb80aaa7acc
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-10 07:44:38 -0600

    create text input and a button

---
+++ strings.js
@@ -0,0 +1,30 @@
+let button = document.getElementById("scramble");
+console.log(button);
+;
+
+button.addEventListener("click", captureText);
+
+function captureText(){
+    let text = document.getElementById("text");
+    let textValue = text.value;
+    console.log(textValue);
+}
+
+
+console.log(text);
+function reversal() {
+    
+    }
+    
+    function alphabits() {
+    
+    }
+    
+    function palindrome() {
+    
+    }
+    
+    var testString = "";
+    reversal(testString);
+    alphabits(testString);
+    palindrome(testString);
