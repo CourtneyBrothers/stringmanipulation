6f3a16687866b40c9f2edf430c99d7db50280e90
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-12 09:08:59 -0600

    reg ex pattern for error handling

--- strings.js
+++ strings.js
@@ -4,45 +4,66 @@ console.log(button);
 
 button.addEventListener("click", captureText);
 
+
+
+
 function captureText(){
     let output = document.getElementById("output");
     let text = document.getElementById("text");
-    let textValue = text.value;
-    console.log(textValue);
+    const re = new RegExp(/\d/g);
+    if (re.test(text.value)){
+        console.log("error");
+    }
+    else {
+        console.log(re.test(text.value));
+        console.log(text.value);
+        console.log("reg worked!");
+    }
     
+                let textValue = text.value;
+                let splitText = textValue.split("");
+                
+                    
+                console.log(textValue);
+                
 
 
 
-    function reversal() {
-       
-        let reversed = textValue.split("").reverse().join("");
-        let reversedText = document.createTextNode(reversed);
-        output.appendChild(reversedText);
-    
-        };
-    
-    function alphabits() {
-        let az = textValue.split("").sort().join("");
-        let azDiv = document.createElement("div");
-        output.appendChild(azDiv);
-        let azText = document.createTextNode(az);
-        azDiv.appendChild(azText);
-    
-        };
-    
-    function palindrome() {
-        palin = textValue.split("").reverse().join("");
-        if (palin === textValue){
-            palindromeDiv = document.createElement("div");
-            output.appendChild(palindromeDiv);
-            palindromeDiv.innerHTML = "Your string is a palindrome";
-        };
+                function reversal() {
+                
+                    let reversed = textValue.split("").reverse().join("");
+                    let reversedText = document.createTextNode(reversed);
+                    output.appendChild(reversedText);
+                
+                    };
+                
+                function alphabits() {
+                    let az = textValue.split("").sort().join("");
+                    let azDiv = document.createElement("div");
+                    output.appendChild(azDiv);
+                    let azText = document.createTextNode(az);
+                    azDiv.appendChild(azText);
+                
+                    };
+                
+                function palindrome() {
+                    palin = textValue.split("").reverse().join("");
+                    if (palin === textValue){
+                        palindromeDiv = document.createElement("div");
+                        output.appendChild(palindromeDiv);
+                        palindromeDiv.innerHTML = "Your string is a palindrome";
+                    };
+                };
+                    
+                    
+                
+                var testString = textValue;
+                console.log(testString);
+                reversal(testString);
+                alphabits(testString);
+                palindrome(testString);
+                }
+             
         
-        } ;  
-    
-    var testString = textValue;
-    console.log(testString);
-    reversal(testString);
-    alphabits(testString);
-    palindrome(testString);
-};
+   
+        
